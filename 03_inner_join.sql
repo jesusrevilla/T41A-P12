bc73df8ef571ca8bc2267cfafb727ff6c96cf82f
VictorCastro135 <178920@upslp.edu.mx>
2025-10-27 19:28:34 -0600

    feature/178920

--- 03_inner_join.sql
+++ 03_inner_join.sql
@@ -1,4 +1,13 @@
 
--- INNER JOIN: solo coincidencias
-SELECT * FROM customers;
-
+SELECT 
+    customers.id,
+    customers.name,
+    customers.country,
+    orders.id AS order_id,
+    orders.product
+FROM 
+    customers
+INNER JOIN 
+    orders
+ON 
+    customers.id = orders.customer_id
