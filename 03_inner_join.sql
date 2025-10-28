2b6da7884bf8f4d7acb4a4d32b2f20469c4ffb9f
Juan Pablo Csnv <175329@upslp.edu.mx>
2025-10-27 19:36:43 -0600

    03_inner_join.sql

--- 03_inner_join.sql
+++ 03_inner_join.sql
@@ -1,4 +1,4 @@
 -- INNER JOIN: solo coincidencias
-SELECT c.name, c.country, o.product
-FROM customers AS c
-INNER JOIN orders AS o ON c.id = o.customer_id;
+SELECT c.name, o.product
+FROM customers c
+INNER JOIN orders o ON c.id = o.customer_id;
