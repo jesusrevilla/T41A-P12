8692d7c5de5966c7bc68bbec538de410f75b8a4f
David Vidales <vidalesrmzdavid@gmail.com>
2025-10-28 11:28:05 -0600

    Update 06_full_outer_join.sql

--- 06_full_outer_join.sql
+++ 06_full_outer_join.sql
@@ -1,2 +1,2 @@
 -- FULL OUTER JOIN: todos los clientes y pedido
-SELECT * FROM customers c FULL OUTER JOIN orders o ON c.id = customer_id;
+SELECT name, product FROM customers c FULL OUTER JOIN orders o ON c.id = customer_id;
