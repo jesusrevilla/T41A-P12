bc58f91162abfaf3f3546c51be76c0370de827d4
Fano-Rdz <178584@upslp.edu.mx>
2025-10-27 19:18:01 -0600

    Implement INNER JOIN to fetch customer and order data

--- 03_inner_join.sql
+++ 03_inner_join.sql
@@ -1,4 +1,6 @@
 
 -- INNER JOIN: solo coincidencias
-SELECT * FROM customers;
+SELECT customers.id, customers.name, customers.country, orders.product
+FROM customers
+INNER JOIN orders ON customers.id = orders.customer_id;
 
