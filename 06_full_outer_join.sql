67c90489390b5072eee2ccf725eaddb6f90bf6fc
SebasMonreal11 <125666778+SebasMonreal11@users.noreply.github.com>
2025-10-28 17:34:24 -0600

    Update 06_full_outer_join.sql

--- 06_full_outer_join.sql
+++ 06_full_outer_join.sql
@@ -1,18 +1,12 @@
 -- FULL OUTER JOIN: todos los clientes y pedido
 SELECT 
-    customers.id AS customer_id,
     customers.name,
-    customers.country,
-    orders.id AS order_id,
     orders.product
 FROM customers
 LEFT JOIN orders ON customers.id = orders.customer_id
 UNION
 SELECT 
-    customers.id AS customer_id,
     customers.name,
-    customers.country,
-    orders.id AS order_id,
     orders.product
 FROM customers
 RIGHT JOIN orders ON customers.id = orders.customer_id;
