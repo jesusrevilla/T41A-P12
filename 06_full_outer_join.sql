801c35e4ef0bb76a09951f52f0c772b642b0661c
Bemused5 <104637264+Bemused5@users.noreply.github.com>
2025-10-27 19:34:24 -0600

    Update 06_full_outer_join.sql
    Implemented a full outer join to retrieve all customers and their orders, including those without orders.

--- 06_full_outer_join.sql
+++ 06_full_outer_join.sql
@@ -1,2 +1,12 @@
 -- FULL OUTER JOIN: todos los clientes y pedido
-SELECT * FROM customers;
+SELECT
+  c.id  AS customer_id,
+  c.name,
+  c.country,
+  o.id  AS order_id,
+  o.product
+FROM customers AS c
+FULL OUTER JOIN orders AS o
+  ON o.customer_id = c.id
+ORDER BY COALESCE(c.id, o.customer_id), COALESCE(o.id, c.id);
+
