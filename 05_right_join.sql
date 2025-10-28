9e1ff588b6e40279e7ff8a66fb3f77891071f641
Bemused5 <104637264+Bemused5@users.noreply.github.com>
2025-10-27 19:31:16 -0600

    Update 05_right_join.sql

--- 05_right_join.sql
+++ 05_right_join.sql
@@ -1,2 +1,11 @@
 -- RIGHT JOIN: todos los pedidos, aunque no tengan cliente
-SELECT * FROM orders;
+SELECT
+  c.id  AS customer_id,
+  c.name,
+  c.country,
+  o.id  AS order_id,
+  o.product
+FROM customers AS c
+RIGHT JOIN orders AS o
+  ON o.customer_id = c.id
+ORDER BY o.id, c.id;
