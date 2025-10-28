c91c892d35364963bacb45bc5052cd1a92010515
Bemused5 <104637264+Bemused5@users.noreply.github.com>
2025-10-27 19:38:14 -0600

    Update 03_inner_join.sql

--- 03_inner_join.sql
+++ 03_inner_join.sql
@@ -1,13 +1,8 @@
-
 -- INNER JOIN: solo coincidencias
 SELECT
-  c.id  AS customer_id,
   c.name,
-  c.country,
-  o.id  AS order_id,
   o.product
 FROM customers AS c
 INNER JOIN orders AS o
   ON o.customer_id = c.id
-ORDER BY c.id, o.id;
-
+ORDER BY c.name, o.id;
