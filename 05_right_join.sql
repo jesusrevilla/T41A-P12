c6015555364ac4ceb273da8a909aec1daf8b72d6
Bemused5 <104637264+Bemused5@users.noreply.github.com>
2025-10-27 19:41:28 -0600

    Update 05_right_join.sql
    Modified the ORDER BY clause to sort by customer name instead of customer ID.

--- 05_right_join.sql
+++ 05_right_join.sql
@@ -1,11 +1,8 @@
 -- RIGHT JOIN: todos los pedidos, aunque no tengan cliente
 SELECT
-  c.id  AS customer_id,
   c.name,
-  c.country,
-  o.id  AS order_id,
   o.product
 FROM customers AS c
 RIGHT JOIN orders AS o
   ON o.customer_id = c.id
-ORDER BY o.id, c.id;
+ORDER BY o.id, c.name;
