b469aac6ef443d81029ff2cd04e84377c792cf41
Bemused5 <104637264+Bemused5@users.noreply.github.com>
2025-10-27 19:42:47 -0600

    Update 06_full_outer_join.sql

--- 06_full_outer_join.sql
+++ 06_full_outer_join.sql
@@ -1,12 +1,8 @@
 -- FULL OUTER JOIN: todos los clientes y pedido
 SELECT
-  c.id  AS customer_id,
   c.name,
-  c.country,
-  o.id  AS order_id,
   o.product
 FROM customers AS c
 FULL OUTER JOIN orders AS o
   ON o.customer_id = c.id
-ORDER BY COALESCE(c.id, o.customer_id), COALESCE(o.id, c.id);
-
+ORDER BY c.name NULLS LAST, o.id;
