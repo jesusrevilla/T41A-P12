742926f6bfa1e7dce632c478be5f7aa44bab58e0
Jpablo179804 <179804@upslp.edu.mx>
2025-10-27 19:27:43 -0600

    Update 03_inner_join.sql

--- 03_inner_join.sql
+++ 03_inner_join.sql
@@ -1,4 +1,7 @@
 
 -- INNER JOIN: solo coincidencias
-SELECT * FROM customers
-  inner join orders;
+--SELECT * FROM customers;
+
+SELECT c.name, o.product
+FROM customers c
+INNER JOIN orders o ON c.id = o.customer_id;
