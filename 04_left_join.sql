208a060fec0df3101bd4d2f8b873ed9e39205282
Bryan Yareth Gámez Contreras <96131248+BryanYarethGC@users.noreply.github.com>
2025-10-27 19:35:43 -0600

    Fix LEFT JOIN to select from customers first

--- 04_left_join.sql
+++ 04_left_join.sql
@@ -1,3 +1,3 @@
 -- LEFT JOIN: todos los clientes, aunque no tengan pedidos
-SELECT customers.name,orders.product FROM orders
-left join customers on customers.id=orders.customer_id;
+SELECT customers.name,orders.product FROM customers
+left join orders on customers.id=orders.customer_id;
