933ac6c1477608b7bbfe3cd0ac9d9cdf985c6d29
Coral024 <202579467+Coral024@users.noreply.github.com>
2025-10-27 19:39:57 -0600

    Update 05_right_join.sql

--- 05_right_join.sql
+++ 05_right_join.sql
@@ -1,2 +1,2 @@
--- RIGHT JOIN: todos los pedidos, aunque no tengan cliente
-SELECT c.name, o.product FROM orders o RIGHT JOIN customers c ON c.id = o.customer_id ;
+s-- RIGHT JOIN: todos los pedidos, aunque no tengan cliente
+SELECT c.name, o.product FROM customers c RIGHT JOIN orders o ON c.id = o.customer_id ;
