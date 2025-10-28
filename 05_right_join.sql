225ab214868cc4093e5aa0edd9480d2cf005c499
Juan Pablo Csnv <175329@upslp.edu.mx>
2025-10-27 19:30:05 -0600

    05_right_join.sql

--- 05_right_join.sql
+++ 05_right_join.sql
@@ -1,2 +1,4 @@
 -- RIGHT JOIN: todos los pedidos, aunque no tengan cliente
-SELECT * FROM orders;
+SELECT c.name, c.country, o.product
+FROM customers AS c
+RIGHT JOIN orders AS o ON c.id = o.customer_id;
