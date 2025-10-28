c7024c5d881432d4d71509d0b874e29ef7b6150c
andreupslpornelas <182085@upslp.edu.mx>
2025-10-27 19:41:48 -0600

    Update 04_left_join.sql

--- 04_left_join.sql
+++ 04_left_join.sql
@@ -1,2 +1,8 @@
--- LEFT JOIN: todos los clientes, aunque no tengan pedidos
+SELECT
+    c.name,
+    o.product
+FROM
+    customers c
+LEFT JOIN
+    orders o ON c.id = o.customer_id;
 SELECT * FROM orders;
