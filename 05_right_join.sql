cfecfd598ddfddaca66eea4c5944a07f065070cf
andreupslpornelas <182085@upslp.edu.mx>
2025-10-27 19:42:05 -0600

    Update 05_right_join.sql

--- 05_right_join.sql
+++ 05_right_join.sql
@@ -1,2 +1,8 @@
--- RIGHT JOIN: todos los pedidos, aunque no tengan cliente
+SELECT
+    c.name,
+    o.product
+FROM
+    customers c
+RIGHT JOIN
+    orders o ON c.id = o.customer_id;
 SELECT * FROM orders;
