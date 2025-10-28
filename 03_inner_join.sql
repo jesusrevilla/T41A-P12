fb45a52cd6c510c2da3296771f60888fe4d424a2
SebasMonreal11 <125666778+SebasMonreal11@users.noreply.github.com>
2025-10-28 17:28:48 -0600

    Add INNER JOIN example for customers and orders
    Updated SQL query to demonstrate INNER JOIN between customers and orders tables.

--- 03_inner_join.sql
+++ 03_inner_join.sql
@@ -1,4 +1,9 @@
-
--- INNER JOIN: solo coincidencias
-SELECT * FROM customers;
-
+-- Inner Join: Combina registros que tienen coincidencias en ambas tablas
+SELECT 
+    customers.id AS customer_id,
+    customers.name,
+    customers.country,
+    orders.id AS order_id,
+    orders.product
+FROM customers
+INNER JOIN orders ON customers.id = orders.customer_id;
