e6e729082ede27ab08772ae84b9a9dd2712ca418
Fano-Rdz <178584@upslp.edu.mx>
2025-10-27 19:19:16 -0600

    Implement FULL OUTER JOIN between customers and orders

--- 06_full_outer_join.sql
+++ 06_full_outer_join.sql
@@ -1,2 +1,4 @@
 -- FULL OUTER JOIN: todos los clientes y pedido
-SELECT * FROM customers;
+SELECT customers.id, customers.name, customers.country, orders.product
+FROM customers
+FULL OUTER JOIN orders ON customers.id = orders.customer_id;
