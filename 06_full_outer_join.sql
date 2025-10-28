16ba053c77e8975825b90fd00ec0392a4f342251
AngelCasti <125672277+AngelCasti@users.noreply.github.com>
2025-10-27 19:37:53 -0600

    Update 06_full_outer_join.sql

--- 06_full_outer_join.sql
+++ 06_full_outer_join.sql
@@ -1,2 +1,2 @@
 -- FULL OUTER JOIN: todos los clientes y pedido
-SELECT customers.*, orders.* FROM customers FULL OUTER JOIN orders ON orders.customer_id = customers.id;
+SELECT customers.name, orders.product FROM customers FULL OUTER JOIN orders ON orders.customer_id = customers.id;
