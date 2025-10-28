c6f866cd2dd007ece287d9510a5ebe36f64315fd
Fano-Rdz <178584@upslp.edu.mx>
2025-10-27 19:18:54 -0600

    Implement RIGHT JOIN to include all orders

--- 05_right_join.sql
+++ 05_right_join.sql
@@ -1,2 +1,4 @@
 -- RIGHT JOIN: todos los pedidos, aunque no tengan cliente
-SELECT * FROM orders;
+SELECT customers.id, customers.name, customers.country, orders.product
+FROM customers
+RIGHT JOIN orders ON customers.id = orders.customer_id;
