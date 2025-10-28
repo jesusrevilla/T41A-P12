8016e5c737b04794005083f56af3b0bf3e053d90
Fano-Rdz <178584@upslp.edu.mx>
2025-10-27 19:28:22 -0600

    Refactor INNER JOIN query for clarity

--- 03_inner_join.sql
+++ 03_inner_join.sql
@@ -1,6 +1,9 @@
 
 -- INNER JOIN: solo coincidencias
-SELECT customers.id, customers.name, customers.country, orders.product
-FROM customers
-INNER JOIN orders ON customers.id = orders.customer_id;
-
+SELECT
+    c.name,
+    o.product
+FROM
+    customers c
+INNER JOIN
+    orders o ON c.id = o.customer_id;
