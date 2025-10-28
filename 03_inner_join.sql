5f586a798d1e55c924a3c4771981f4d83b442726
Palau003 <178561@upslp.edu.mx>
2025-10-27 19:41:29 -0600

    Update 03_inner_join.sql

--- 03_inner_join.sql
+++ 03_inner_join.sql
@@ -1,4 +1,7 @@
 -- INNER JOIN: solo coincidencias
-SELECT c.name, o.product
-FROM customers c
-INNER JOIN orders o ON c.id = o.customer_id;
+SELECT 
+    customers.name,
+    orders.product
+FROM customers
+INNER JOIN orders
+    ON customers.id = orders.customer_id;
