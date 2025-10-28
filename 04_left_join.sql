9c7638becf91b00102bc2f611d2676bdf03a45be
Fano-Rdz <178584@upslp.edu.mx>
2025-10-27 19:18:29 -0600

    Implement LEFT JOIN to include all customers

--- 04_left_join.sql
+++ 04_left_join.sql
@@ -1,2 +1,4 @@
 -- LEFT JOIN: todos los clientes, aunque no tengan pedidos
-SELECT * FROM orders;
+SELECT customers.id, customers.name, customers.country, orders.product
+FROM customers
+LEFT JOIN orders ON customers.id = orders.customer_id;
