79ba9152f94c1154b17ffd5a8f3a5f23a307de5e
SebasMonreal11 <125666778+SebasMonreal11@users.noreply.github.com>
2025-10-28 17:33:45 -0600

    Update 04_left_join.sql

--- 04_left_join.sql
+++ 04_left_join.sql
@@ -1,9 +1,6 @@
 -- LEFT JOIN: todos los clientes, aunque no tengan pedidos
 SELECT 
-    customers.id AS customer_id,
     customers.name,
-    customers.country,
-    orders.id AS order_id,
     orders.product
 FROM customers
 LEFT JOIN orders ON customers.id = orders.customer_id;
