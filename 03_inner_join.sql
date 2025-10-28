f33b919a44ca6b3cd5dc94744b22b7848359c2c2
SebasMonreal11 <125666778+SebasMonreal11@users.noreply.github.com>
2025-10-28 17:33:23 -0600

    Update 03_inner_join.sql

--- 03_inner_join.sql
+++ 03_inner_join.sql
@@ -1,9 +1,6 @@
 -- Inner Join: Combina registros que tienen coincidencias en ambas tablas
 SELECT 
-    customers.id AS customer_id,
     customers.name,
-    customers.country,
-    orders.id AS order_id,
     orders.product
 FROM customers
 INNER JOIN orders ON customers.id = orders.customer_id;
