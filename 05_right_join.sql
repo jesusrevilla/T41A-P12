cda1df9bfabfc92c4cae59800657377004577e5a
YaelQCI <181914@upslp.edu.mx>
2025-10-27 19:41:04 -0600

    Update 05_right_join.sql

--- 05_right_join.sql
+++ 05_right_join.sql
@@ -1,2 +1,2 @@
 -- RIGHT JOIN: todos los pedidos, aunque no tengan cliente
-SELECT * FROM orders o RIGHT JOIN customers c ON o.customer_id=c.id;
+SELECT c.name, o.id FROM customers c  RIGHT JOIN orders o ON o.customer_id=c.id;
