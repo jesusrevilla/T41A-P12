dae32bdcb6000a7b352ba649e332b08fd7e31631
andreupslpornelas <182085@upslp.edu.mx>
2025-10-27 19:40:51 -0600

    Update 02_insert_data.sql

--- 02_insert_data.sql
+++ 02_insert_data.sql
@@ -1,7 +1,2 @@
-SELECT
-    c.name,
-    o.product
-FROM
-    customers c
-INNER JOIN
-    orders o ON c.id = o.customer_id;
+\copy customers FROM 'customers.csv' DELIMITER ',' CSV HEADER;
+\copy orders FROM 'orders.csv' DELIMITER ',' CSV HEADER;
