93ea24bd2ea006ccf29ae5f95839a8e80d5855c7
menaxixi <179800@upslp.edu.mx>
2025-10-28 19:14:32 -0600

    Update 06_full_outer_join.sql

--- 06_full_outer_join.sql
+++ 06_full_outer_join.sql
@@ -1,2 +1,5 @@
 -- FULL OUTER JOIN: todos los clientes y pedido
-SELECT * FROM customers;
+--SELECT * FROM customers;
+SELECT customers.id, customers.name, customers.country, orders.id AS order_id, orders.product
+FROM customers
+FULL OUTER JOIN orders ON customers.id = orders.customer_id;
