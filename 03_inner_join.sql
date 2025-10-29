4d596f99e17dbda2e5cda5bcd11052cece0e63e5
menaxixi <179800@upslp.edu.mx>
2025-10-28 19:13:55 -0600

    Update 03_inner_join.sql

--- 03_inner_join.sql
+++ 03_inner_join.sql
@@ -1,4 +1,7 @@
 
 -- INNER JOIN: solo coincidencias
-SELECT * FROM customers;
+--SELECT * FROM customers;
+SELECT customers.id, customers.name, customers.country, orders.id AS order_id, orders.product
+FROM customers
+INNER JOIN orders ON customers.id = orders.customer_id;
 
