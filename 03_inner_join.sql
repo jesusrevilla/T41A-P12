46c1e84c37df98646f3f99359c9962f6b0420514
SayraIbarra <130393897+SayraIbarra@users.noreply.github.com>
2025-10-31 16:57:10 -0600

    Update 03_inner_join.sql

--- 03_inner_join.sql
+++ 03_inner_join.sql
@@ -1,4 +1,8 @@
-
--- INNER JOIN: solo coincidencias
 SELECT * FROM customers;
+SELECT 
+    customers.name,
+    orders.product
+FROM customers
+INNER JOIN orders
+    ON customers.id = orders.customer_id;
 
