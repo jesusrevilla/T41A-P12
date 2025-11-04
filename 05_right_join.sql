bdafe6fe4225e53dda4691343d2413b6b4a4f7cb
Cristian R. Godinez Limones <cristianlimones3@gmail.com>
2025-11-03 19:11:54 -0600

    Modify RIGHT JOIN query to include customer details

--- 05_right_join.sql
+++ 05_right_join.sql
@@ -1,2 +1,11 @@
--- RIGHT JOIN: todos los pedidos, aunque no tengan cliente
-SELECT * FROM orders;
+-- RIGHT JOIN: todos los pedidos, con su cliente si existe
+SELECT
+    c.customer_id,
+    c.name,
+    o.order_id,
+    o.order_date,
+    o.amount
+FROM customers AS c
+RIGHT JOIN orders AS o
+    ON o.customer_id = c.customer_id
+ORDER BY o.order_id;
