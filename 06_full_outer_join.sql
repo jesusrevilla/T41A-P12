5c5f42b2d6309137bfdf38a5dfedf1a9a10e59e2
Cristian R. Godinez Limones <cristianlimones3@gmail.com>
2025-11-03 19:18:18 -0600

    Update 06_full_outer_join.sql

--- 06_full_outer_join.sql
+++ 06_full_outer_join.sql
@@ -1,13 +1,4 @@
--- FULL OUTER JOIN: todos los clientes y todos los pedidos
-SELECT
-    c.customer_id,
-    c.name,
-    o.order_id,
-    o.order_date,
-    o.amount
-FROM customers AS c
-FULL OUTER JOIN orders AS o
-    ON o.customer_id = c.customer_id
-ORDER BY
-    COALESCE(c.customer_id, -2147483648),
-    COALESCE(o.order_id, -2147483648);
+SELECT c.name, p.product
+FROM customers c
+FULL OUTER JOIN products p
+    ON c.customer_id = p.customer_id;
