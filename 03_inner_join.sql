e0f06a5a624275356b4442375a25b266d2cccdb8
Cristian R. Godinez Limones <cristianlimones3@gmail.com>
2025-11-03 19:11:09 -0600

    Refactor INNER JOIN query to select specific columns
    Modified INNER JOIN query to select specific fields and added ordering.

--- 03_inner_join.sql
+++ 03_inner_join.sql
@@ -1,4 +1,12 @@
-
--- INNER JOIN: solo coincidencias
-SELECT * FROM customers;
+-- INNER JOIN: solo clientes con pedidos
+SELECT
+    c.customer_id,
+    c.name,
+    o.order_id,
+    o.order_date,
+    o.amount
+FROM customers AS c
+INNER JOIN orders AS o
+    ON o.customer_id = c.customer_id
+ORDER BY c.customer_id, o.order_id;
 
