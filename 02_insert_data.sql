9bb130b6983628b543c3595dc560640db296ab4a
JuanpiMarinD <126593527+JuanpiMarinD@users.noreply.github.com>
2025-11-18 10:54:34 -0600

    Add customer and product insertion SQL commands
    Inserted customer and product data directly into the database.

--- 02_insert_data.sql
+++ 02_insert_data.sql
@@ -1,5 +1,8 @@
+INSERT INTO customers (customer_id, name) VALUES
+(1, 'Ana'),
+(2, 'Luis'),
+(3, 'Carlos');
 
--- Cargar datos desde CSV
-\copy customers FROM 'customers.csv' DELIMITER ',' CSV HEADER;
-\copy orders FROM 'orders.csv' DELIMITER ',' CSV HEADER;
-
+INSERT INTO products (product_id, product, customer_id) VALUES
+(1, 'Phone', 1),
+(2, 'Laptop', 2);
